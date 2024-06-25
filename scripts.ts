9fa9fb80d5273ac60735e4c368e444570b3bd15d
Mark <kotleta2007@protonmail.ch>
2024-06-25 10:57:27 +0200

    Parsing integrations to get actions

--- scripts.ts
+++ scripts.ts
@@ -61,11 +61,74 @@ export async function getScripts(integration: string) {
 
   const packages = JSON.parse(Buffer.from(content, "base64").toString("utf8"));
   const dependencies = removeDependencies(packages, "activepieces");
-  console.log(packages)
+  console.log(packages);
   console.log(dependencies);
+
+  // actions
+  const { data: data2 } = await octokit.repos.getContent({
+    owner,
+    repo,
+    path: `packages/pieces/community/${integration}/src/lib/actions`,
+  });
+
+  if (!Array.isArray(data2)) {
+    throw new Error("Invalid integration folder structure");
+  }
+
+  console.log("data2: ", data2)
+
+  const actionIds = data2
+    .map((d) => d.name);
+
+  console.log("actionIds: ", actionIds)
+
+  const actions: {
+    id: string;
+    name: string | undefined;
+    description: string | undefined;
+  }[] = [];
+
+  for (const actionId of actionIds) {
+    let data;
+    const response = await octokit.repos.getContent({
+      owner,
+      repo,
+      path: `packages/pieces/community/${integration}/src/lib/actions/${actionId}`,
+    });
+      data = response.data;
+    if (Array.isArray(data) || data.type !== "file") {
+      continue;
+    }
+
+    const { content } = data;
+    const code = Buffer.from(content, "base64").toString("utf8");
+
+    console.log(code);
+
+    const match = code.match(/displayName: '(.+)'/);
+    const name = match?.[1];
+    const match2 = code.match(/description: '(.+)'/);
+    const description = match2?.[1];
+
+    actions.push({
+      name: name,
+      description: description,
+      id: actionId,
+    });
+  }
+
+  console.log(actions);
+ 
+
+  // triggers
+  
+
   return packages
 }
 
 // getNewIntegrations(10).then(integrations => console.log(integrations));
 getScripts("claude").then(packages => packages);
 
+// go into src/lib/
+// find all the files that are *.ts
+//
